a0895248eb7063c2a821fe24d1ed9e77de9a9763
yoonmiriii <gotjrak1357@gmail.com>
2024-04-25 09:43:00 +0900

    자동차 가격 예측

--- eda.py
+++ eda.py
@@ -22,9 +22,19 @@ def run_eda() :
 
     # 각 컬럼별로 최대/최소 값을 보여주는 화면 개발.
     # 유저가 컬럼을 선택하면, 해당 컬럼의 최대/최소 데이터를 보여주도록 하자.
-
     
     st.text('컬럼을 선택하면, 각 컬럼별 최대/최소 데이터를 보여드립니다.')
     column_list = ['Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
-    st.selectbox('컬럼을 선택하세요', column_list)
+    choice_column = st.selectbox('컬럼을 선택하세요', column_list)
+
+    st.info(f'선택하신 {choice_column}의 최대 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].max(), ])
+
+    st.info(f'선택하신 {choice_column}의 최소 데이터는 다음과 같습니다.')
+    st.dataframe(df.loc[df[choice_column] == df[choice_column].min(), ])
+
+    st.subheader('상관관계 분석')
+    st.text('컬럼들을 2개 이상 선택하면, 컬럼들의 상관계수를 보여드립니다.')
+
+
 
